da3dccb2cc9648e14c86d9cd91297a7a135eb9f3
Scott <lemmingapex@gmail.com>
2017-10-03 19:24:04 +0000

    stupid code

---
+++ motion_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+led_output_pin=13
+motion_input_pin=11
+GPIO.setup(motion_input_pin, GPIO.IN)
+GPIO.setup(led_output_pin, GPIO.OUT)
+while True:
+	i=GPIO.input(motion_input_pin)
+	if i == 1:
+		GPIO.output(led_output_pin, GPIO.HIGH)
+		print "Motion!"
+	else:
+		GPIO.output(led_output_pin, GPIO.LOW)
+	time.sleep(0.05)
