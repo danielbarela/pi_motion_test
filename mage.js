8f126d595b09b3ee11a7052ef9d323e7ec322d69
Dan Barela <daniel.barela@gmail.com>
2017-10-04 11:27:01 -0600

    adding mage

---
+++ mage.js
@@ -0,0 +1,79 @@
+var request = require('request');
+
+var mageEvent = 1;
+var token;
+
+module.exports.login = function(callback) {
+  request.post({
+    url: process.env.MAGE_URL + '/api/login',
+    form: {
+      username: process.env.MAGE_USER,
+      uid: process.env.MAGE_UID,
+      password: process.env.MAGE_PASSWORD
+    }
+  }, function(err, response, body) {
+    console.log('got a token', response);
+    token = response.token;
+  });
+}
+
+module.exports.getId = function(callback) {
+  request.post({
+    url: process.env.MAGE_URL + '/api/events/' + mageEvent + '/observations/id',
+    form: {
+      eventId: mageEvent
+    },
+    headers: {
+      Authorization: 'Bearer ' + token
+    },
+  }, function(err, response, body) {
+      if (err) callback(err);
+      var id = response.id;
+      callback(err, id);
+    }
+  );
+}
+
+module.exports.sendObservation = function(observation, callback) {
+  request.post({
+    url: process.env.MAGE_URL + '/api/events/' + mageEvent + '/observations/id/' + observation.id,
+    form: observation,
+    headers: {
+      Authorization: 'Bearer ' + token
+    }
+  }, function(err, response, body) {
+    console.log('sent the observation', response);
+  });
+}
+
+module.exports.newObservation = function(id, lat, lng, sensorType) {
+    return {
+      id: id,
+      eventId: mageEvent,
+      type: 'Feature',
+      geometry: {
+        type: 'Point',
+        coordinates: [lng, lat]
+      },
+      properties: {
+        timestamp: new Date().toISOString(),
+        type: sensorType
+      }
+    };
+}
+
+module.exports.setDistance = function(form, distance) {
+  form.properties.field4 = distance;
+}
+
+// https://mageiot.geointservices.io/api/events/1/observations/id
+// request:
+// {eventId: 1}
+//
+// response: {"__v":0,"id":"59d50426607cb672fa04d17f","eventId":1,"url":"https://mageiot.geointservices.io/api/events/1/observations/59d50426607cb672fa04d17f"}
+//
+// https://mageiot.geointservices.io/api/events/1/observations/id/59d50426607cb672fa04d17f
+// request:
+// {"eventId":1,"type":"Feature","geometry":{"type":"Point","coordinates":[-77.16796875,41.902277040963696]},"properties":{"timestamp":"2017-10-04T15:54:00.922Z","type":"Motion","field4":5},"options":{"layerId":"NewObservation","selected":true,"draggable":true},"id":"59d50426607cb672fa04d17f"}
+// response:
+// {"lastModified":"2017-10-04T15:54:14.361Z","userId":"59d4fb4a607cb672fa04d17d","deviceId":"59d42c0a686dc66397164017","type":"Feature","geometry":{"type":"Point","coordinates":[-77.16796875,41.902277040963696]},"properties":{"timestamp":"2017-10-04T15:54:00.922Z","type":"Motion","field4":5},"__v":0,"createdAt":"2017-10-04T15:54:14.361Z","favoriteUserIds":[],"attachments":[],"id":"59d50426607cb672fa04d17f","eventId":1,"url":"https://mageiot.geointservices.io/api/events/1/observations/59d50426607cb672fa04d17f","state":{"userId":"59d4fb4a607cb672fa04d17d","name":"active","id":"59d50426607cb672fa04d180","url":"https://mageiot.geointservices.io/api/events/1/observations/59d50426607cb672fa04d17f/states/59d50426607cb672fa04d180"}}
--- motion-picture.js
+++ motion-picture.js
@@ -1,44 +1,57 @@
 #!/usr/bin/env node
 
-var rpio = require('rpio');
-var RaspiCam = require("raspicam");
-var tmp = require('tmp');
-var path = require('path');
+// Set environment variables MAGE_USER and MAGE_URL and MAGE_PASSWORD to communicate with MAGE
+
+var rpio = require('rpio')
+  , RaspiCam = require("raspicam")
+  , tmp = require('tmp')
+  , path = require('path')
+  , Mage = require('./mage');
+
+var led_output_pin = 13;
+var motion_input_pin = 11;
+
+rpio.open(motion_input_pin, rpio.INPUT);
+rpio.open(led_output_pin, rpio.OUTPUT);
 
 initializeMotionSensor();
 
-function initializeMotionSensor() {
-  var led_output_pin = 13;
-  var motion_input_pin = 11;
 
-  rpio.open(motion_input_pin, rpio.INPUT);
-  rpio.open(led_output_pin, rpio.OUTPUT);
-
-  rpio.poll(motion_input_pin, function(pin) {
-    var isMotion = rpio.read(motion_input_pin);
-    if (isMotion) {
-      rpio.write(led_output_pin, rpio.HIGH);
-      console.log('Motion!');
-      takeAPicture();
-    } else {
-      rpio.write(led_output_pin, rpio.LOW);
-      console.log('Motion complete');
-    }
-  });
+function sendMAGEObservation() {
+  var form = Mage.newObservation();
 }
 
+function initializeMotionSensor() {
+  rpio.poll(motion_input_pin, motionChange);
+}
+
+function motionChange(pin) {
+  var isMotion = rpio.read(motion_input_pin);
+  if (isMotion) {
+    rpio.poll(motion_input_pin, null);
+    rpio.write(led_output_pin, rpio.HIGH);
+    console.log('Motion!');
+    takeAPicture();
+  } else {
+    rpio.write(led_output_pin, rpio.LOW);
+    console.log('Motion complete');
+  }
+}
 
 function takeAPicture() {
   var filePath = tmp.tmpNameSync() + '.jpg';
   var camera = new RaspiCam({
     mode: "photo",
     output: filePath,
-    rotation: 270
+    rotation: 0,
+    t: 5
   });
 
   camera.on('read', function() {
     camera.stop();
     console.log('Picture written to %s', filePath);
+    // schedule the motion sensor polling to begin again in 30 seconds
+    setTimeout(initializeMotionSensor, 30000);
   });
 
   camera.start();
