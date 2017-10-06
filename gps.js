1af44aec510eae19c34c39b6f708a2dfc3d1ab0b
William Newman <newmanw10@gmail.com>
2017-10-06 14:25:39 -0600

    Start gps daemon from code

--- gps.js
+++ gps.js
@@ -1,6 +1,20 @@
 var gpsd = require('node-gpsd')
   , fs = require('fs');
 
+
+var daemon = new gpsd.Daemon({
+    program: 'gpsd',
+    device: '/dev/ttyS0',
+    port: 2947,
+    pid: '/tmp/gpsd.pid',
+    readOnly: false,
+    logger: {
+        info: console.log,
+        warn: console.warn,
+        error: console.error
+    }
+});
+
 var listener = new gpsd.Listener({
   port: 2947,
   hostname: 'localhost',
@@ -12,29 +26,33 @@ var listener = new gpsd.Listener({
   parse: true
 });
 
-listener.connect(function() {
-  console.log('GPS deamon connected');
-});
+daemon.start(function() {
+  console.log('GPS Daemon Started');
 
-listener.on('TPV', function (tpv) {
-  console.log('gps recevied, writing to filesystem');
-  var geojson = {
-     geometry: {
+  listener.connect(function() {
+    console.log('GPS deamon connected');
+  });
+
+  listener.on('TPV', function (tpv) {
+    console.log('gps recevied, writing to filesystem');
+    var geojson = {
+      geometry: {
         type: 'Point',
         coordinates: [tpv.lon, tpv.lat]
-     },
-     properties: {
-       timestamp: tpv.time,
-       altitude: tpv.alt,
-       speed: tpv.speed
-     }
-  };
+      },
+      properties: {
+        timestamp: tpv.time,
+        altitude: tpv.alt,
+        speed: tpv.speed
+      }
+    };
 
-  fs.writeFileSync('./db/location.json',  JSON.stringify(geojson), 'utf-8'); 
-});
+    fs.writeFileSync('./db/location.json',  JSON.stringify(geojson), 'utf-8'); 
+  });
 
-listener.watch({
-  class: 'WATCH',
-  json: true,
-  nmea: false
+  listener.watch({
+    class: 'WATCH',
+    json: true,
+    nmea: false
+  });
 });
